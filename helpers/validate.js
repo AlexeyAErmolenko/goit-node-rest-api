e41d486ca34ea3b9f042dcfd0b95499aa16dab35
Olexii Yermolenko <AlexeyErmolenko@i.ua>
2025-08-08 17:25:58 +0300

    Write code

---
+++ helpers/validate.js
@@ -0,0 +1,25 @@
+import HttpError from "./HttpError.js";
+
+export const validateBody = (schema) => {
+  const func = (req, _, next) => {
+    const { error } = schema.validate(req.body);
+    if (error) {
+      next(HttpError(400, error.message));
+    }
+    next();
+  };
+  return func;
+};
+
+export const validateId = (schema) => {
+  const func = (req, _, next) => {
+    const { error } = schema.validate(req.params);
+    if (error) {
+      next(HttpError(400, error.message));
+    }
+    next();
+  };
+  return func;
+};
+
+// export default validate;
